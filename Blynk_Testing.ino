e48fabac46b40731f5606c476ce8e3b06ebd54e2
Juan Ruiz <41452350+JuanRuiz213@users.noreply.github.com>
2019-01-22 22:55:36 -0600

    Update Blynk_Testing.ino

--- Blynk_Testing.ino
+++ Blynk_Testing.ino
@@ -2,23 +2,27 @@
 #include <BlynkSimpleSerialBLE.h>
 #include <SoftwareSerial.h>
 
+//THIS IS THE ONLY CODE YOU SHOULD CHANGE ACCORDING TO YOUR DIGITAL CONNECTIONS
+//----------------------------------------------
 int in1 = 8;
 int in2 = 9;
 
 int in3 = 10;
 int in4 = 11;
 
+//DO NOT CHANCE THESE, CONNECT THE RX TO DIGITAL PIN 2 AND TX TO DIGITAL PIN 3
 int RX = 2;
 int TX = 3;
+//----------------------------------------------
+
 SoftwareSerial SwSerial(RX, TX); // RX, TX
-
-
-
+  
+//MAKE SURE TO USE YOUR AUTH KEY GIVEN BY THE BLYNK APP
+//----------------------------------------------
 char auth[] = "INSERT YOUR OWN KEY HERE";
+//----------------------------------------------
 
 SoftwareSerial SerialBLE(RX, TX); // RX, TX
-
-
 
 void setup()
 {
